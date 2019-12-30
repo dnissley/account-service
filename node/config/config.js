6609748500eb572bc0b5591c96f5fe8d4e18cdc9
Dylan Nissley <dylan.nissley@gmail.com>
2019-12-30 16:45:09 -0500

    More shuffling

--- node/config/config.js
+++ node/config/config.js
@@ -1,20 +1,20 @@
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "accounts",
+    username: process.env.DB_USER || "accounts",
     password: process.env.DB_PASSWORD || "password",
     database: process.env.DB_DATABASE || "accounts",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql"
   },
   test: {
-    username: process.env.DB_USERNAME || "accounts",
+    username: process.env.DB_USER || "accounts",
     password: process.env.DB_PASSWORD || "password",
     database: process.env.DB_DATABASE || "accounts",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
