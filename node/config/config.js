143c29ba73c8ef1fab2359576731bbf21d7672d4
Dylan Nissley <dylan.nissley@gmail.com>
2019-12-30 16:39:55 -0500

    Shuffling things around

---
+++ node/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || "accounts",
+    password: process.env.DB_PASSWORD || "password",
+    database: process.env.DB_DATABASE || "accounts",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: process.env.DB_USERNAME || "accounts",
+    password: process.env.DB_PASSWORD || "password",
+    database: process.env.DB_DATABASE || "accounts",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  }
+};
